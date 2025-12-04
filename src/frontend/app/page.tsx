a90676c62f91d00e9699d4cf196864e29bfb22e7
Petr Ungar <petr.ungar@gmail.com>
2025-12-04 18:41:55 +0100

    remove resizable

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -3,33 +3,23 @@
 import { PlaybackProvider, PlaybackControls, usePlayback } from "@features/playback";
 import { SimulationApp } from "@features/simulation";
 import { MetricsPanel } from "@features/metrics";
-import {
-  ResizablePanelGroup,
-  ResizablePanel,
-  ResizableHandle,
-} from "@/components/ui/resizable";
 
 function HomeContent() {
   const { hasRecording } = usePlayback();
 
   return (
     <div className="flex flex-col w-full h-screen">
-      {/* Main Content: Resizable Panels */}
-      <div className="flex-1 min-h-0 w-full">
-        <ResizablePanelGroup direction="horizontal">
-          {/* Left Panel: Simulation */}
-          <ResizablePanel defaultSize={75} minSize={50}>
-            <SimulationApp />
-          </ResizablePanel>
+      {/* Main Content: Fixed Split Panels */}
+      <div className="flex-1 min-h-0 w-full flex">
+        {/* Left Panel: Simulation (70%) */}
+        <div className="w-[70%] h-full">
+          <SimulationApp />
+        </div>
           
-          {/* Resize Handle */}
-          <ResizableHandle withHandle />
-          
-          {/* Right Panel: Metrics */}
-          <ResizablePanel defaultSize={25} minSize={20}>
-            <MetricsPanel />
-          </ResizablePanel>
-        </ResizablePanelGroup>
+        {/* Right Panel: Metrics (30%) */}
+        <div className="w-[30%] h-full border-l border-gray-200">
+          <MetricsPanel />
+        </div>
       </div>
       
       {/* Bottom Navigation Bar */}
