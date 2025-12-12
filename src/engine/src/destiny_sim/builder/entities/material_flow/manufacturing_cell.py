8b21f4553f2a8ecb5239e2e71aa247fd29698348
Filip Kubiš <fkubis@icloud.com>
2025-12-12 20:28:59 +0100

    update api types

--- src/engine/src/destiny_sim/builder/entities/material_flow/manufacturing_cell.py
+++ src/engine/src/destiny_sim/builder/entities/material_flow/manufacturing_cell.py
@@ -49,6 +49,7 @@ class ManufacturingCell(BuilderEntity):
         self.buffer_out = buffer_out
         self.mean = mean
         self.std_dev = std_dev
+        self._box = Box()
 
     def process(self, env: RecordingEnvironment):
         """
@@ -103,11 +104,9 @@ class ManufacturingCell(BuilderEntity):
 
         processing_duration = process_duration - flow_in_duration - flow_out_duration
 
-        box = Box()
-
         # Visualize material flow in
         env.record_motion(
-            box,
+            self._box,
             start_x=self.buffer_in.x,
             start_y=self.buffer_in.y,
             end_x=self.x,
@@ -117,7 +116,7 @@ class ManufacturingCell(BuilderEntity):
 
         # Visualize processing
         env.record_stay(
-            box,
+            self._box,
             x=self.x,
             y=self.y,
             start_time=env.now + flow_in_duration,
@@ -126,7 +125,7 @@ class ManufacturingCell(BuilderEntity):
 
         # Visualize material flow out
         env.record_motion(
-            box,
+            self._box,
             start_x=self.x,
             start_y=self.y,
             end_x=self.buffer_out.x,
