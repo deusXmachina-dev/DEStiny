70ed35909f3c9559f904a313ade4e18455abb462
Filip Kubiš <fkubis@icloud.com>
2025-12-14 16:01:25 +0100

    add manufacturing cell icon

--- src/frontend/features/visualization/constants.ts
+++ src/frontend/features/visualization/constants.ts
@@ -13,7 +13,7 @@ export const ASSET_MAP: Record<string, string> = {
   source: "/assets/source.png",
   sink: "/assets/sink.png",
   buffer: "/assets/buffer.png",
-  manufacturing_cell: "/assets/robot.png",
+  manufacturing_cell: "/assets/manufacturing_cell.png",
   control: "/assets/control.png",
 } satisfies Partial<Record<SimulationEntityType, string>>;
 
