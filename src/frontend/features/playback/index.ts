9afca5a913f1a5c19b294a690e73d866ce5fe630
Petr Ungar <petr.ungar@gmail.com>
2025-12-15 18:19:09 +0100

    fix non-existing import

--- src/frontend/features/playback/index.ts
+++ src/frontend/features/playback/index.ts
@@ -1,6 +1,6 @@
 export { PlaybackControls } from "./components/PlaybackControls";
 export { PlaybackProvider, usePlayback } from "./hooks/PlaybackContext";
-export { usePlaybackState, useTimePolling } from "./hooks/usePlaybackState";
+export { usePlaybackState } from "./hooks/usePlaybackState";
 export { PlaybackClock } from "./PlaybackClock";
 export { formatTime } from "@lib/utils";
 
